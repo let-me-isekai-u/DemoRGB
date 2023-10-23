543208d3263a47e493ca0fe39ab69ccd3be51d73
let-me-isekai-u <141901221+let-me-isekai-u@users.noreply.github.com>
2023-10-23 22:57:31 +0700

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -31,7 +31,7 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_c
 # Trang chủ
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('RGBcontrol.html')
 
 # API để nhận dữ liệu từ cảm biến DHT22 và điều khiển đèn RGB và LCD
 @app.route('/update', methods=['POST'])
